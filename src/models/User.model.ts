edd64cdb0f3b7c8f8b7a02703cfa57f52304e2fd
Soumipal56 <soumiisc2020@gmail.com>
2026-06-05 12:59:51 +0530

    feat: implement user model, registration API route, and JWT authentication utilities

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -1,6 +1,8 @@
+import { IUser } from "@/types/user.types"
 import mongoose from "mongoose"
+import bcrypt from 'bcrypt'
 
-let userSchema = new mongoose.Schema({
+let userSchema = new mongoose.Schema<IUser>({
     name: {
         type: String,
         trim: true,
@@ -16,5 +18,25 @@ let userSchema = new mongoose.Schema({
         type: String,
         required: [true, "Password is required"],
         minlength: [6, "Password must be at least 6 characters long"]
+    },
+    mobile: {
+        type: String,
+        minlength: [10, "Minimum 10 characters required"],
+        maxlength: [10, "Maximum 10 characters required"]
     }
+}, {
+    timestamps: true
 })
+
+userSchema.pre('save', function () {
+    if(!this.isModified('password')) return
+    this.password = bcrypt.hashSync(this.password, 10)
+})
+
+userSchema.methods.comparePass = function(candidatePassword: string) {
+    return bcrypt.compareSync(candidatePassword, this.password)
+}
+
+const userModel = mongoose.model('User', userSchema)
+
+export default userModel
